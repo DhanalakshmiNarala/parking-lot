ced50502ae220d777d865408f62f2db405373ead
Dhanalakshmi Narala <dhanalakshmi.narala@mable.com.au>
2024-09-12 15:11:31 +0530

    Add vehicle parking functionality for parking lot

--- src/models/ParkingLot.ts
+++ src/models/ParkingLot.ts
@@ -1,11 +1,38 @@
+import { ParkingSpot } from './ParkingSpot';
+import { Vehicle } from './Vehicle';
+
 export class ParkingLot {
   private capacity: number;
+  private spots: ParkingSpot[];
 
   constructor(capacity: number) {
     this.capacity = capacity;
+    this.spots = this.createParkingSpots();
+  }
+
+  private createParkingSpots(): ParkingSpot[] {
+    const spots: ParkingSpot[] = [];
+    for (let i = 0; i < this.capacity; i++) {
+      spots[i] = new ParkingSpot(i + 1, true);
+    }
+    return spots;
   }
 
   getCapacity(): number {
     return this.capacity;
+  }
+
+  getParkingSpots(): ParkingSpot[] {
+    return this.spots;
+  }
+
+  parkVehicle(vehicle: Vehicle): number {
+    for (let i = 0; i < this.capacity; i++) {
+      if (this.spots[i].isAvailable()) {
+        this.spots[i].assignVehicle(vehicle);
+        return this.spots[i].getPosition();
+      }
+    }
+    return -1;
   }
 }
--- tests/models/ParkingLot.test.ts
+++ tests/models/ParkingLot.test.ts
@@ -1,8 +1,30 @@
 import { ParkingLot } from '../../src/models/ParkingLot';
+import { Vehicle } from '../../src/models/Vehicle';
 
 describe('Parking Lot', () => {
-  it('should create parking lot with the given capacity', () => {
+  it('should create parking lot with the given capacity ', () => {
     const parkingLot = new ParkingLot(5);
     expect(parkingLot.getCapacity()).toBe(5);
+  });
+
+  it('Should create parking spots with default availability true', () => {
+    const parkingLot = new ParkingLot(5);
+
+    const spots = parkingLot.getParkingSpots();
+    for (let i = 0; i < 5; i++) {
+      expect(spots[i].isAvailable()).toBe(true);
+    }
+  });
+
+  it('Should park vehicle at the earlier spot', () => {
+    const parkingLot = new ParkingLot(5);
+    const vehicleOne = new Vehicle('xyz-123', 'White');
+    const vehicleTwo = new Vehicle('abc-123', 'Black');
+
+    const vehicleOnePosition = parkingLot.parkVehicle(vehicleOne);
+    const vehicleTwoPosition = parkingLot.parkVehicle(vehicleTwo);
+
+    expect(vehicleOnePosition).toBe(1);
+    expect(vehicleTwoPosition).toBe(2);
   });
 });
