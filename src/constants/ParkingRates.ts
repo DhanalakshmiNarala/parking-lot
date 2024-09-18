a82c4d038db690bd30b55a1e59a786246e471831
Dhanalakshmi Narala <dhanalakshmi.narala@mable.com.au>
2024-09-18 17:53:54 +0530

    Fix cost calculator module import issues

--- src/constants/ParkingRates.ts
+++ src/constants/ParkingRates.ts
@@ -1,4 +1,4 @@
-const BASE_RATE = 50;
-const BASE_RATE_DURATION_IN_HOURS = 0.5; // 30 mins
+export const BASE_RATE = 50;
+export const BASE_RATE_DURATION_IN_HOURS = 0.5; // 30 mins
 
-const PER_HOUR_RATE = 100; // Rate for after initial 30 mins
+export const PER_HOUR_RATE = 100; // Rate for after initial 30 mins
--- src/utils/CostCalculator.ts
+++ src/utils/CostCalculator.ts
@@ -1,3 +1,9 @@
+import {
+  BASE_RATE_DURATION_IN_HOURS,
+  BASE_RATE,
+  PER_HOUR_RATE,
+} from '../constants/ParkingRates';
+
 export const calculateCostOfParkingHours = (hours: number) => {
   if (hours <= BASE_RATE_DURATION_IN_HOURS) {
     return BASE_RATE;
